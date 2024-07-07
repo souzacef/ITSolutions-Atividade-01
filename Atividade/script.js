9d7323b175f6e7fe7ef529667b8f5a72a4c2f8ea
souzacef <125619939+souzacef@users.noreply.github.com>
2024-07-06 22:55:08 -0300

    Switched focusout for a button + visual adjustments

--- Atividade/script.js
+++ Atividade/script.js
@@ -1,30 +1,32 @@
-window.onload=function(){
-    document.getElementById("altura").addEventListener("focusout",function(){
-       peso = document.getElementById("peso").value;
-       altura = document.getElementById("altura").value;
-
-       resultado = (parseFloat(peso) 
-       / ((parseFloat(altura) / 100) * (parseFloat(altura) / 100)))
-       .toFixed(2);
-
-       document.getElementById("resultado").value = resultado;
-
-       if(resultado < 18.5) {
-        document.getElementById("classificacao").value = "Abaixo do peso";
-       }else if(resultado >= 18.5 && resultado < 24.9) {
-        document.getElementById("classificacao").value="Peso normal";
-       }else if(resultado >= 25 && resultado < 29.9) {
-        document.getElementById("classificacao").value="Sobrepeso";
-       }else{
-        document.getElementById("classificacao").value="Obesidade";
-       }     
-
-    })
-}
-
-document.getElementById("limpar").addEventListener("click", function(){
-    document.getElementById("peso").value = "";
-    document.getElementById("altura").value = "";
-    document.getElementById("resultado").value = "";
-    document.getElementById("classificacao").value = "";
-})
+function calcularIMC() {
+    peso = document.getElementById("peso").value;
+    altura = document.getElementById("altura").value;
+  
+    
+    if (peso === "" || altura === "") {
+      alert("Por favor, preencha os campos de peso e altura!");
+      return; 
+    }
+  
+    resultado = (parseFloat(peso) / ((parseFloat(altura) / 100) * (parseFloat(altura) / 100))).toFixed(2);
+  
+    let mostraImc = document.getElementById("mostra-imc");
+    mostraImc.textContent = "Seu IMC é: " + resultado;
+  
+    if (resultado < 18.5) {
+      document.getElementById("classificacao").value = "Abaixo do peso";
+    } else if (resultado >= 18.5 && resultado < 24.9) {
+      document.getElementById("classificacao").value = "Peso normal";
+    } else if (resultado >= 25 && resultado < 29.9) {
+      document.getElementById("classificacao").value = "Sobrepeso";
+    } else {
+      document.getElementById("classificacao").value = "Obesidade";
+    }
+  
+    console.log("Resultado do IMC:", resultado);
+    console.log("Classificação:", document.getElementById("classificacao").value);
+    
+    
+    event.preventDefault();
+  }
+  
